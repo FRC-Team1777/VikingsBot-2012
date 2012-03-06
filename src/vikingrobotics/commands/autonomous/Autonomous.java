89e2b2b4df2c834fe4a0cf18c21c61105dc92412
Neal <neal@ineal.me>
2012-03-06 06:40:10 -0600

    Get autonomous setting from SmartDashboard

--- src/vikingrobotics/commands/autonomous/Autonomous.java
+++ src/vikingrobotics/commands/autonomous/Autonomous.java
@@ -9,8 +9,12 @@ package vikingrobotics.commands.autonomous;
 
 import vikingrobotics.commands.CommandBase;
 import vikingrobotics.misc.Constants;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.PrintCommand;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Autonomous
@@ -19,15 +23,29 @@ import edu.wpi.first.wpilibj.command.PrintCommand;
  */
 public class Autonomous extends CommandGroup implements Constants {
 	
+	private SendableChooser autonChooser;
+	private Command AutonCommand;
+	
 	public Autonomous() {
 		super("Autonomous");
+		AutonCommand = getAutonCommdand();
 		if (CommandBase.oi.getDS().getDS().getDigitalIn(kDSDigitalInputAutonomous)) {
-			addSequential(new PrintCommand("[autonomous] Running auton1"));
-			addSequential(new Auton1());
+			addSequential(new PrintCommand("[autonomous] Running " + AutonCommand));
+			addSequential(AutonCommand);
 		}
 		else {
 			addSequential(new PrintCommand("[autonomous] Digital Input 8 returned false; skipping Autonomous"));
 		}
+	}
+	
+	public Command getAutonCommdand() {
+		autonChooser = new SendableChooser();
+		autonChooser.addDefault("Auton0", new WaitCommand(1.0));
+		autonChooser.addObject("Auton1", new Auton1());
+		autonChooser.addObject("Auton2", new Auton2());
+		autonChooser.addObject("Auton3", new Auton3());
+		SmartDashboard.putData("Autonomous Setting", autonChooser);
+		return (Command) autonChooser.getSelected();
 	}
 
 }
